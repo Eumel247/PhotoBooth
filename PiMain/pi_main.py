f7a1dbf98c01ae10ffbcb2c502f76f74afc8880b
Eumel247 <jerome-schaetzle@online.de>
2014-04-24 16:32:33 +0200

    pi reaction on gertduino pushbutton

--- PiMain/pi_main.py
+++ PiMain/pi_main.py
@@ -5,17 +5,19 @@ ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=1)
 print "pi_main.py running";
 
 
-while True: #Test with PiGertduinoSerial.ino
-    inStr = raw_input("Enter an led number between 0 and 5 followed by ENTER ")
-    try:
-        led = int(inStr)
-    except ValueError:
-        led = -1
-    if led >= 0 and led <= 5:
-        ser.write(str(led))
-        print ser.readline()[:-2]
+#while True: #Test with PiGertduinoSerial.ino
+#    inStr = raw_input("Enter an led number between 0 and 5 followed by ENTER ")
+#    try:
+#        led = int(inStr)
+#    except ValueError:
+#        led = -1
+#    if led >= 0 and led <= 5:
+#        ser.write(str(led))
+#        print ser.readline()[:-2]
 
-	
+while True:
+	#ser.write(str(led))
+        print ser.readline()[:-2]
 		
 def pi_getpicture():
    "switches USB on and fetches the latest picture"
