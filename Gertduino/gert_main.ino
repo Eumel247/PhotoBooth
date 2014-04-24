2506ab3f9cfc2d27daaa069b025ad66968065a07
Eumel247 <jerome-schaetzle@online.de>
2014-04-24 16:02:49 +0200

    Basic Gertduino code - Pushbuttons

--- Gertduino/gert_main.ino
+++ Gertduino/gert_main.ino
@@ -1,15 +1,71 @@
---DSLR PhotoBooth--
+/*
+ Input Pullup Serial
+ 
+ This example demonstrates the use of pinMode(INPUT_PULLUP). It reads a
+ digital input on pin 2 and prints the results to the serial monitor.
+ 
+ The circuit:
+ * Momentary switch attached from pin 2 to ground
+ * Built-in LED on pin 13
+ 
+ Unlike pinMode(INPUT), there is no pull-down resistor necessary. An internal
+ 20K-ohm resistor is pulled to 5V. This configuration causes the input to
+ read HIGH when the switch is open, and LOW when it is closed.
+ 
+ created 14 March 2012
+ by Scott Fitzgerald
+ 
+ http://www.arduino.cc/en/Tutorial/InputPullupSerial
+ 
+ This example code is in the public domain
+ 
+ */
 
-Developed for a wedding: Boxdesign on a speaker stand, with pushbuttons, screen, printer, 7-segement display, ... as userinterface.
+void setup(){
+  Serial.begin(9600); //start serial connection
+  
+  int shutterBUT = A2; 	//left push-button
+  pinMode(shutterBUT, INPUT_PULLUP); // enable the internal pull-up resistor
+  
+  int printBUT = A3;	//right push-button
+  pinMode(printBUT, INPUT_PULLUP);
+  
+  int shutterLED = 13;	//LED0
+  pinMode(shutterLED, OUTPUT);
+  
+  int printLED = 9;		//LED1
+  pinMode(printLED, OUTPUT);
+  
+  //int LED = 10;		//LED2
+  //int LED = 4;		//LED3
+  //int LED = 6;		//LED4
+  //int LED = 7;		//LED5
+  
+}
 
-The System is controlled by a Raspberry in combination with a Gertduino.
-The used DSLR is an Olympus E-510, triggered by cable (Gertduino). The flash is integrated in a white box.
-Token pictures are transfered to the Raspberry by USB (USB has to be switched on an off, because the connected USB disables the Live-View).
-Live-View of the DSLR is converted from AV to VGA and is displayed on a TFT screen.
-It is possible to print the pictures (merged with existing pictures) on a Canon Selphy CP800 and review the existing pictures on an Ipad (backside of the Box).
-
-The Picture-Taking-Process is initialized by a push on an customized "Easy-Button" which is connected to the Arduino and starts the main loop. 
-
-
-...
-
+void loop(){
+  //read the pushbutton values into variables
+  int shutterVal = digitalRead(shutterBUT);
+  int printVal = digitalRead(printBUT);
+  
+  //print out the values of the pushbuttons  
+  Serial.println(shutterVal);
+  Serial.println(printVal);
+ 
+  // Keep in mind the pullup means the pushbutton's
+  // logic is inverted. It goes HIGH when it's open,
+  // and LOW when it's pressed. Turn on shutterLED when the
+  // button's pressed, and off when it's not:
+  if (shutterVal == HIGH) {
+    digitalWrite(shutterLED, LOW);
+  }
+  else {
+    digitalWrite(shutterLED, HIGH);
+  }
+  if (printVal == HIGH) {
+    digitalWrite(printLED, LOW);
+  }
+  else {
+    digitalWrite(printLED, HIGH);
+  }
+}
--- PiMain/pi_main.py
+++ PiMain/pi_main.py
@@ -1,15 +1,41 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+import serial #serial communication on?
+ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=1)
 
 print "pi_main.py running";
 
-#Initialize 
 
-#import pi_photomerge
-#import pi_print
-#import pi_getpicture
+while True: #Test with PiGertduinoSerial.ino
+    inStr = raw_input("Enter an led number between 0 and 5 followed by ENTER ")
+    try:
+        led = int(inStr)
+    except ValueError:
+        led = -1
+    if led >= 0 and led <= 5:
+        ser.write(str(led))
+        print ser.readline()[:-2]
 
-#Loop: Wait for Userinteraction:
-#- Pushbutton: Make Photo
-#- Pushbutton: Print
+	
+		
+def pi_getpicture():
+   "switches USB on and fetches the latest picture"
+   #tell Gertduino to close the USB+ relay
+   #automount usb
+   #transfer picture to extHDD
+   #eject /media/KINGSTON;
+   #tell Gertduino to open the relay
+   return;
+   
+   
+def pi_photomerge():
+	"merges the latest picture(s) into a printable <Photostripe>"
+	#
+	return
+
+   
+def pi_print():
+	"run after pi_photomerge! prints to Canon Selphy 800"
+	#- Pushbutton: Print
+	return
 
 
