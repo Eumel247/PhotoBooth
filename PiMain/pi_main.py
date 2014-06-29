4299846d2f44ee72604ea8a9a15789ed16fdd01e
Eumel247 <jerome-schaetzle@online.de>
2014-06-29 15:01:25 +0200

    play around with pi_main.py

--- PiMain/pi_main.py
+++ PiMain/pi_main.py
@@ -19,27 +19,28 @@ while True:
     data = ser.read(9999)
     if len(data) > 0:
         print "Received:", data
-	if data=="gert2pi_shutter":
-    		print "execute shutter"
-  	elif data=="gert2pi_print":
-    		print "execute print"
-    	elif data=="gert2pi_getPicture":
-    		print "getPicture"
-  	else:
-		print "command not known"
+	if "gert2pi_shutter" in data:
+    		print "execute shutter";
+  	elif "gert2pi_print" in data:
+    		print "execute print";
+		pi_print();
+    	elif "gert2pi_getPicture" in data:
+    		print "getPicture";
+  	else:	
+		print "command not known";
   	
     #time.sleep(0.5)
 
 
 		
 def pi_getpicture():
-   "switches USB on and fetches the latest picture"
-   #tell Gertduino to close the USB+ relay
-   #automount usb
-   #transfer picture to extHDD
-   #eject /media/KINGSTON;
-   #tell Gertduino to open the relay
-   return
+   	"switches USB on and fetches the latest picture"
+	#tell Gertduino to close the USB+ relay
+   	#automount usb
+   	#transfer picture to extHDD
+   	#eject /media/KINGSTON;
+   	#tell Gertduino to open the relay
+   	return
    
    
 def pi_photomerge():
@@ -50,5 +51,6 @@ def pi_photomerge():
    
 def pi_print():
 	"run after pi_photomerge! prints to Canon Selphy 800"
+	print "druck...";
 	#- Pushbutton: Print
 	return
